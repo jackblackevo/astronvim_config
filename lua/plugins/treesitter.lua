21d7f6dd88dd266b68733ce6c934c97764dfaf06
ryan.c <jackblackevo@gmail.com>
2024-05-02 14:33:27 +0800

    Install rainbow delimiters

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,6 +5,10 @@
 ---@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    -- https://github.com/HiPhish/rainbow-delimiters.nvim
+    { "HiPhish/rainbow-delimiters.nvim" },
+  },
   opts = function(_, opts)
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
