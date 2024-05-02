c42bb13b6a7513c8b28572550cb3a2e16c5dcafb
ryan.c <jackblackevo@gmail.com>
2024-05-02 14:34:56 +0800

    Open alpha-nvim when session load fail

--- lua/plugins/git_branch_sessions.lua
+++ lua/plugins/git_branch_sessions.lua
@@ -56,7 +56,13 @@ return {
             -- Only load the session if nvim was started with no args
             if vim.fn.argc(-1) == 0 then
               -- try to load a directory session using the current working directory
-              require("resession").load(get_session_name(), { dir = "dirsession", silence_errors = true })
+              -- https://github.com/stevearc/resession.nvim/blob/4463a78e1783c99466d5f5fb79516d06a5d6f3f8/lua/resession/init.lua#L485-L490
+              xpcall(require("resession").load, function()
+                -- https://github.com/AstroNvim/AstroNvim/blob/e2edcc7e197d577912d29305f67d7c995ae47353/lua/astronvim/plugins/alpha.lua#L68-L70
+                require("lazy").load { plugins = { "alpha-nvim" } }
+                require("alpha").start(true)
+                vim.schedule(function() vim.cmd.doautocmd "FileType" end)
+              end, get_session_name(), { dir = "dirsession" })
             end
           end,
         },
