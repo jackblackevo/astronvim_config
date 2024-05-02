1c24632f26a9afce11e5802562594a1982dec364
ryan.c <jackblackevo@gmail.com>
2024-05-02 14:31:52 +0800

    Install TypeScript all-in-one language pack

--- lua/community.lua
+++ lua/community.lua
@@ -9,4 +9,6 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
+  -- https://github.com/AstroNvim/astrocommunity/tree/main/lua/astrocommunity/pack/typescript-all-in-one
+  { import = "astrocommunity.pack.typescript-all-in-one" },
 }
