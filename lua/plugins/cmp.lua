0dbfc4317975b040fde4db3639b07eb3f2bbd080
ryan.c <jackblackevo@gmail.com>
2024-05-02 14:31:08 +0800

    Pretty printing cmp menu items for cmp-tabnine

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -3,6 +3,8 @@
 return {
   "hrsh7th/nvim-cmp",
   dependencies = {
+    -- https://github.com/onsails/lspkind.nvim
+    { "onsails/lspkind.nvim" },
     -- add cmp source as dependency of cmp
     { "tzachar/cmp-tabnine", build = "./install.sh" },
   },
@@ -35,6 +37,36 @@ return {
         cmp.config.compare.length,
         cmp.config.compare.order,
       },
+    }
+
+    -- https://github.com/tzachar/cmp-tabnine#pretty-printing-menu-items
+    local lspkind = require "lspkind"
+
+    local source_mapping = {
+      buffer = "[Buffer]",
+      nvim_lsp = "[LSP]",
+      nvim_lua = "[Lua]",
+      cmp_tabnine = "[TN]",
+      path = "[Path]",
+    }
+
+    opts.formatting = {
+      format = function(entry, vim_item)
+        -- if you have lspkind installed, you can use it like
+        -- in the following line:
+        vim_item.kind = lspkind.symbolic(vim_item.kind, { mode = "symbol" })
+        vim_item.menu = source_mapping[entry.source.name]
+        if entry.source.name == "cmp_tabnine" then
+          local detail = (entry.completion_item.labelDetails or {}).detail
+          vim_item.kind = ""
+          if detail and detail:find ".*%%.*" then vim_item.kind = vim_item.kind .. " " .. detail end
+
+          if (entry.completion_item.data or {}).multiline then vim_item.kind = vim_item.kind .. " " .. "[ML]" end
+        end
+        local maxwidth = 80
+        vim_item.abbr = string.sub(vim_item.abbr, 1, maxwidth)
+        return vim_item
+      end,
     }
   end,
 }
